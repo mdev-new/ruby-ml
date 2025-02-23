16125d64624ef83d8d2f06045c298e4c34b222b0
mdev-new <mdev-new@users.noreply.github.com>
2025-02-23 21:50:56 +0100

    Progress bar

--- main.rb
+++ main.rb
@@ -15,18 +15,31 @@ layers = [
 net = NeuralNetwork.new optimizer, layers, :BinaryCrossEntropy
 net.randomize
 
-# 1000 epochs
-for i in 0..10000
-  #        A  B  A^B !A^B
+def progress_bar(current, total, length = 20)
+  percent = (current.to_f / total) * 100
+  progress = ((current.to_f / total) * length).to_i
+
+  bar = "=" * progress + " " * (length - progress)
+  percent_str = "#{percent.round(1)}%"
+  start_index = [(length - percent_str.length) / 2, 0].max
+  bar[start_index, percent_str.length] = percent_str
+
+  bar
+end
+
+epochs = 100000
+for i in 0..epochs
+  bar = progress_bar(i, epochs)
+
+  print "\r[#{bar}]"
+
   net.fit input[0], output[0]
   net.fit input[1], output[1]
   net.fit input[2], output[2]
   net.fit input[3], output[3]
-
-  # rnd = rand(0..3)
-  # net.validate input[rnd], output[rnd]
-
 end
+
+print "\n"
 
 p (net.predict [1,0])[0].round 8
 p (net.predict [0,0])[0].round 8
